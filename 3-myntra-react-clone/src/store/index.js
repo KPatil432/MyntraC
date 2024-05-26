7e6fd0e9f6466a8a9f289a21abbdf848d8b3e7b5
KPatil432 <142484297+KPatil432@users.noreply.github.com>
2024-05-26 12:27:56 +0530

    Add files via upload

---
+++ 3-myntra-react-clone/src/store/index.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import itemsSlice from "./ItemsSlice";
+import fetchStatusSlice from "./FetchStatusSlice";
+import bagSlice from "./bagSlice";
+
+
+
+
+const myntraStore = configureStore({
+  reducer: {
+    items: itemsSlice.reducer,
+    fetchStatus: fetchStatusSlice.reducer,
+    bag: bagSlice.reducer,
+  },
+});
+
+export default myntraStore;
